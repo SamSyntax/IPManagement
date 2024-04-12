320180e6943c5fa47d4280a40d833bdf4b7621ab
SamSyntax <klfracc@gmail.com>
2024-04-12 15:01:23 +0200

    Trying build fix

--- app/api/assignIp/route.ts
+++ app/api/assignIp/route.ts
@@ -89,6 +89,7 @@ export async function GET(req: Request, res: Response) {
     // Fetch users with their associated IP addresses
     const ips = await prisma.iPAddress.findMany({});
     res.ok;
+    req.json();
 
     // Return the users with a success status
     return NextResponse.json(ips, { status: 200 });

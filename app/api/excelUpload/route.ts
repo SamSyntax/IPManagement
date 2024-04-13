1cbf861087e087b1091c739eabb85d58de8947fd
SamSyntax <klfracc@gmail.com>
2024-04-13 09:19:01 +0200

    fixing up build

--- app/api/excelUpload/route.ts
+++
@@ -1,7 +0,0 @@
-import multer from "multer";
-
-import * as nextConnect from "next-connect";
-
-const uploadMiddleware = multer({ dest: "./" });
-
-const apiRoute = nextConnect<NextApiRequest, NextApiResponse>({});

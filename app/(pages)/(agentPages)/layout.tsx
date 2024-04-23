95de737f070c4e4b2afb74e7b7ba2839ce39d29b
SamSyntax <klfracc@gmail.com>
2024-04-23 14:19:19 +0200

    themechange

--- app/(pages)/(agentPages)/layout.tsx
+++ app/(pages)/(agentPages)/layout.tsx
@@ -1,32 +1,17 @@
-import type { Metadata } from "next";
-import { GeistSans } from "geist/font/sans";
 import "@/app/globals.css";
-import Nav from "@/components/nav/Nav";
 import { ThemeProvider } from "@/providers/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 import React, { Suspense } from "react";
 import PageLoading from "../(root)/loading";
 
-const geist = GeistSans;
-export const metadata: Metadata = {
-  title: "DBS IP Management",
-  description: "DB Schenker VPN IP Management application",
-};
-
-
 const AgentLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang="en">
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        <body className={geist.className}>
-          <Nav />
-          <main className="overflow-x-hidden">
-            <Suspense fallback={<PageLoading />}> {children}</Suspense>
-          </main>
-          <Toaster />
-        </body>
-      </ThemeProvider>
-    </html>
+    <section>
+      <main className="overflow-x-hidden">
+        <Suspense fallback={<PageLoading />}> {children}</Suspense>
+      </main>
+      <Toaster />
+    </section>
   );
 };
 export default AgentLayout;
